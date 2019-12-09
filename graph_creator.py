bd803a0ee739500e354444edb8216c8f3061121e
teo99516 <40856983+teo99516@users.noreply.github.com>
2019-12-09 20:05:33 +0200

    Added edges to graph

--- graph_creator.py
+++ graph_creator.py
@@ -4,32 +4,76 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
-path=os.getcwd()+'\keras\\tests'
-
 #Returns all the paths of the files in a directory
 #Starts from the files in the directory and then from the directories top bottom 
-file_paths = []
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.py' in file:
-            file_paths.append(os.path.join(r, file))
+def get_all_paths(path):
 
-library,keywords=tokenize_files.getLibsandKeywords(file_paths[0])
+    file_paths = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.py' in file:
+                file_paths.append(os.path.join(r, file))
+    return file_paths
 
-print(library)
-print(keywords)
+if __name__ == "__main__": 
 
-G=nx.Graph()
-index=0
-for string in library:
-    G.add_node( index,library=string)
-    index=index+1 
+    dir_path=os.getcwd()+'\keras\\tests'
 
-print(G.nodes(data=True))
-#pos=nx.spring_layout(G)
+    file_paths= get_all_paths(dir_path)
+    libraries=[]
+    keywords=[]
+    G=nx.Graph()
+
+    index=1
+    for file_path in file_paths:
+        path_libraries,path_keywords=tokenize_files.get_libs_and_keywords(file_path)
+        
+        for string in path_keywords:
+            if (len(string)==2):
+                if  string[-1].isdigit():
+                    path_keywords.remove(string)
+            #if(len(string)==1):
+             #   path_keywords.remove(string)
 
 
-nx.draw(G)
+        print("Path Tokenazation {0} of {1} has finished".format(str(index), str(len(file_paths))),str(index/len(file_paths)*100),"%","of 100%")
+        index=index+1
 
-plt.show()
+        libraries = list(set(libraries + path_libraries))
+        keywords = list(set(keywords + path_keywords))
+
+        #print(library)
+        #print(keywords)
+        
+        for string in  path_libraries:
+            if string not in libraries:
+                G.add_node(string)
+        for string in path_keywords:
+            if string not in keywords:
+                G.add_node(string) 
+
+        for string in  path_libraries:
+            for string2 in path_keywords:
+
+                G.add_edge(string,string2)
+
+    #print("Number of unique libraries:",len(libraries))
+    #print("Libraries listed alphabetically:")
+    #libraries.sort()
+    #print(libraries)
+    
+    print("Number of unique keywords:",len(keywords))
+    keywords.sort()
+    print("Keywords listed alphabetically:")
+    print(keywords)
+
+    #print(G.nodes(data=True))
+    #print("Nodes of graph: ")
+    #print(G.nodes())
+    #print("Edges of graph: ")
+    #print(G.edges())
+
+    #nx.draw(G,with_labels=True)
+
+    #plt.show()
 
--- tokenize_files.py
+++ tokenize_files.py
@@ -1,13 +1,39 @@
 import os
 import re
-import matplotlib.pyplot as plt
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+import spacy
+from spacy.tokens import Doc
+from cube.api import Cube
+from nltk.corpus import stopwords 
 
+#cube=Cube(verbose=True)
+#cube.load("en")
 
-def getLibsandKeywords(path):
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+nlp=spacy.load('en_core_web_sm')
 
+stop_words = set(stopwords.words('english')) 
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
+def get_libs_and_keywords(path):
+
+    
     file=open(path,'r')
     #file_as_text=file.read()
-
+    lemmatizer = WordNetLemmatizer()
     library=[]
     keywords=[]
     for line in file:
@@ -15,13 +41,61 @@ def getLibsandKeywords(path):
         line_text = line_text.split()
         if ('import' or 'from' or 'as') in line_text:
             for string in line_text:
-                if string!='import' and string!='from' and string!='as'and (string not in library):
-                    library.append(string)
+                #if string.endswith('.'):              
+                    #string.replace('.', '')
+               
+                if string!='import' and string!='from' and string!='as'and (string.lower() not in library)and (len(string)>1):                   
+                    library.append(string.lower())
         else:
             for string in line_text:
-                #Check if the string contains characters and if it already exists
-                if any(c.isalpha() for c in string) and (string not in keywords):
-                    keywords.append(string)
+                
+                if string.endswith('.'):
+                    string=string.translate({ord('.'): None})
+                if string.startswith('.'):
+                    string=string.translate({ord('.'): None})
+                if(string.endswith("_")):
+                    string=string.translate({ord('_'): None})
+                if(string.startswith("_")):
+                    string=string.translate({ord('_'): None})
+                if(string.endswith("`")):
+                    string=string.translate({ord('`'): None})
+                if(string.startswith("`")):
+                    string=string.translate({ord('`'): None})
+                #Check if the string contains characters and if it already exists(ignore case)
+                if any(c.isalpha() for c in string) and (string.lower() not in keywords) and (len(string)>1):
+                    if not(string.isdigit()):
+                        if string not in stop_words:               
+                            keywords.append(string.lower())
+
+    
+    for string in keywords:
+        #Remove strings like y1 etc.
+        if (len(string)==2):
+           if any(c.isdigit() for c in string):
+                keywords.remove(string)
+        #Remove string like y_1, y_b etc.
+        if (len(string)==3 and string[1]=='_'):
+            keywords.remove(string)
+
+
+        
+            
+
+
+   # new=[]
+   # for word in keywords:
+    #    sentences=cube(word)
+     #   for sentence in sentences:
+      #      for entry in sentence:
+       #         new.append(str(entry.lemma))
+       
+    #library = [cube(word).lemma for word in library]
+    #keywords= [ for word in keywords]
+
+    doc_library = Doc(nlp.vocab, words=library)
+    doc_keywords = Doc(nlp.vocab, words=keywords)
+    library = [word.lemma_ for word in doc_library]
+    keywords= [word.lemma_ for word in doc_keywords]
 
     return library, keywords
 
@@ -38,7 +112,7 @@ if __name__ == "__main__":
             if '.py' in file:
                 file_paths.append(os.path.join(r, file))
 
-    library,keywords=getLibsandKeywords(file_paths[0])
+    library,keywords=get_libs_and_keywords(file_paths[0])
     print(library)
     print(keywords)
 
