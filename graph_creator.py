dbdd83e92ec7da54fd8a82c853b4fff5c14b124b
teo99516 <40856983+teo99516@users.noreply.github.com>
2019-12-17 17:47:32 +0200

    Add weights for co-occurencies Remove '.' and '_' from the keywords

--- graph_creator.py
+++ graph_creator.py
@@ -4,87 +4,91 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#Returns all the paths of the files in a directory
-#Starts from the files in the directory and then from the directories top bottom 
-def get_all_paths(path):
 
+# Returns all the paths of the files in a directory
+# Starts from the files in the directory and then from the directories top bottom
+def get_all_paths(dir_path):
     file_paths = []
-    for r, d, f in os.walk(path):
-        for file in f:
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
             if '.py' in file:
-                file_paths.append(os.path.join(r, file))
+                file_paths.append(os.path.join(root, file))
+
     return file_paths
 
-if __name__ == "__main__": 
 
-    dir_path=os.getcwd()+'\keras\\tests'
+def remove_unwanted_words(keywords):
+    # Matches a string with 1 to 3 numbers, zero or more "." and one or 2 letters
+    pattern = re.compile(r'[0-9]{1,3}.*?[a-z]{1,2}')
+    for keyword in keywords:
+        if pattern.match(keyword):
+            keywords.remove(keyword)
+    # Remove strings like y1, 1y etc.
+    for keyword in keywords:
+        if len(keyword) == 2:
+            if any(c.isdigit() for c in keyword):
+                keywords.remove(keyword)
 
-    file_paths= get_all_paths(dir_path)
-    libraries=[]
-    keywords=[]
-    G=nx.Graph()
+    return keywords
+
+
+if __name__ == "__main__":
+
+    dir_path = os.getcwd() + '\keras\\tests'
+    file_paths = get_all_paths(dir_path)
+    libraries = []
+    keywords = []
+    G = nx.Graph()
 
     for file_path in file_paths:
+        # Get the path's libraries and keyword for the specific file in the path
+        path_libraries, path_keywords = tokenize_files.get_libs_and_keywords(file_path)
 
-        path_libraries,path_keywords=tokenize_files.get_libs_and_keywords(file_path)
+        index = file_paths.index(file_path) + 1
+        print("Path Tokenizing {0} of {1} has finished".format(str(index), str(len(file_paths))),
+              str(index / len(file_paths) * 100), "%", "of 100%")
 
-        index=file_paths.index(file_path)+1
-        print("Path Tokenazation {0} of {1} has finished".format(str(index), str(len(file_paths))),str(index/len(file_paths)*100),"%","of 100%")
+        # Check if an edge exits in the graph. If so, then increase weight by one
+        G.add_weighted_edges_from([(library, keyword, int(G.get_edge_data(library, keyword)['weight']) + 1)
+                                   for library in path_libraries for keyword in path_keywords
+                                   if G.has_edge(library, keyword)])
+
+        # Connect each library with all the keywords(nodes are creates automatically if they don't' exist on the graph)
+        G.add_weighted_edges_from([(library, keyword, 1) for library in path_libraries for keyword in path_keywords
+                                   if not (G.has_edge(library, keyword))])
 
         libraries = list(set(libraries + path_libraries))
         keywords = list(set(keywords + path_keywords))
-       
-       #Add libraries as nodes in the graph
-        for string in  path_libraries:
-            if string not in libraries:
-                G.add_node(string)
 
-        #Add keyword as nodes in the graph
-        for string in path_keywords:
-            if string not in keywords:
-                G.add_node(string) 
+    keywords = remove_unwanted_words(keywords)
 
-        #Connect each library with all the keyowords
-        for string in  path_libraries:
-            for string2 in path_keywords:
-                G.add_edge(string,string2)
-
-    #Matches a string with 1 to 3 numbers, zero or more "." and one or 2 letters
-    pattern=re.compile(r'[0-9]{1,3}.*?[a-z]{1,2}')
-    for string in keywords:
-        if( pattern.match(string)):
-            keywords.remove(string)
-        #if (string.isdigit()):
-            #keywords.remove(string)
-        if ( len(string)==1):
-             keywords.remove(string)
-
-    for key in keywords:
-        #Remove strings like y1, 1y etc.
-        if (len(key)==2):
-           if any(c.isdigit() for c in key):
-                keywords.remove(key)
-        #Remove string like y_1, y_b etc.
-        if (len(key)==3 and key[1]=='_'):
-            keywords.remove(key)  
-
-    print("Number of unique libraries:",len(libraries))
+    print("Number of unique libraries: ", len(libraries))
     print("Libraries listed alphabetically:")
     libraries.sort()
     print(libraries)
 
-    print("Number of unique keywords:",len(keywords))
-    keywords.sort()
+    print("Number of unique keywords: ", len(keywords))
     print("Keywords listed alphabetically:")
+    keywords.sort()
     print(keywords)
 
-    #print(G.nodes(data=True))
-    #print("Nodes of graph: ")
-    #print(G.nodes())
-    #print("Edges of graph: ")
-    #print(G.edges())
+    print("Number of Nodes in the graph: ", len(G.nodes()))
+    print("Number of Edges in the graph: ", len(G.edges()))
 
-    #nx.draw(G,with_labels=True)
+    # Create a subgraph with only edges with large values
+    max_subgraph = nx.Graph()
+    for library in libraries:
+        for keyword in keywords:
+            if G.has_edge(library, keyword):
+                label = G.get_edge_data(library, keyword)
+                if int(label['weight']) > 20:
+                    max_subgraph.add_edge(library, keyword, weight=int(label['weight']))
 
-    #plt.show()
+    # Get the weights for each edge
+    labels = nx.get_edge_attributes(max_subgraph, 'weight')
+    # Draw the subgraph network(with large weight values) using labels for the nodes
+    nx.draw_networkx(max_subgraph, pos=nx.spring_layout(max_subgraph), with_labels=True)
+    # Draw the labels of the edges
+    edge_labels = nx.draw_networkx_edge_labels(max_subgraph, pos=nx.spring_layout(max_subgraph), edge_labels=labels)
 
+    plt.show()
--- tokenize_files.py
+++ tokenize_files.py
@@ -2,100 +2,112 @@ import os
 import re
 import nltk
 import spacy
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
 from spacy.tokens import Doc
 from spacy.lang.en import English
-from spacy.lang.en.stop_words import STOP_WORDS
 
 nlp_eng = English()
-nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
-nlp=spacy.load('en_core_web_sm',disable=['parser', 'ner'])
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
 
 def get_libs_and_keywords(path):
-  
-    file=open(path,'r')
-    libraries=[]
-    keywords=[]
-    lib_dict={}
 
-    for line in file:
+    python_file = open(path, 'r')
+    libraries = []
+    keywords = []
+    libraries_dict = {}
 
-        line_text = re.sub(r"\(|\)|\:|\[|\]|\"|\'|\||\\|\{|\}|\=|\+|\-|\*|\/|\%|\,|\<|\>", " ", line)
-        line_text = line_text.split()
-        
-        if ('import' or 'from' or 'as') in line_text:
-            
-            #Store the values of the libraries that was imported as something else in a dictionary 
-            if 'as' in line_text:
-                lib_dict[line_text[line_text.index('as')+1].lower()]=line_text[line_text.index('import')+1]
-                #Remove string after 'as', keep only the imported library
-                line_text.pop(line_text.index('as')+1)
+    for code_line in python_file:
 
-            for string in line_text:  
-                if string!='import' and string!='from' and string!='as'and (string.lower() not in libraries):                   
-                    libraries.append(string.lower())
+        if ('import' or 'from' or 'as') in code_line:
+
+            # Removes unwanted characters by replacing them with spaces
+            line_code_as_text = re.sub(r"\(|\)|\:|\[|\]|\"|\'|\||\\|\{|\}|\=|\+|\-|\*|\/|\%|\<|\>|\@|\!|\`|\,",
+                                       " ", code_line)
+            # Split the text line by line
+            splitted_code_line = line_code_as_text.split()
+
+            # Get the library's full name and store it to a dictionary
+            libraries_full_name, libraries_dict = parse_lines_with_libraries(splitted_code_line, libraries_dict)
+            libraries = libraries + libraries_full_name
 
         else:
-            for string in line_text:
+            line_code_as_text = re.sub(r"\(|\)|\:|\.|\[|\]|\"|\'|\||\\|\{|\}|\=|\+|\-|\*|\/|\%|\.|\,|\<|\>|\_|\@|\!|\`",
+                                       " ", code_line)
+            splitted_code_line = line_code_as_text.split()
 
-                string=string.lower()
-                #Removes unwanted characters from the keyword strings
-                if string.endswith('.'):
-                    string=string.translate({ord('.'): None})
-                if string.startswith('.'):
-                    string=string.translate({ord('.'): None})
-                if(string.endswith("_")):
-                    string=string.translate({ord('_'): None})
-                if(string.startswith("_")):
-                    string=string.translate({ord('_'): None})
-                if(string.endswith("`")):
-                    string=string.translate({ord('`'): None})
-                if(string.startswith("`")):
-                    string=string.translate({ord('`'): None})
-                if(string.endswith("!")):
-                    string=string.translate({ord('!'): None})
+            for keyword in splitted_code_line:
 
-                #Replace the libraries that was imported as a different name with the real library name
-                #(key-dot should exist in the string in order for the found key to be the actual library and not just a character in a string)
-                for key in lib_dict.keys():
-                    if (key+'.')in string :                      
-                        string=string.replace( key, lib_dict[key] )
-                    
-                #Check if the string contains characters and if it already exists(ignore case)
-                if not(string.isdigit()):
-                    if any(c.isalpha() for c in string) and (string not in keywords) and (len(string)>1):
-                            if not(nlp.vocab[string].is_stop):               
-                                keywords.append(string)
-   
-    #Spacy tokens lemmatization
-    doc_keywords = Doc(nlp.vocab, words=keywords)
-    keywords= [word.lemma_ for word in doc_keywords]
+                keyword = keyword.lower()
+                # Replace the libraries that was imported as a different name with the real library name
+                for key in libraries_dict.keys():
+                    if key == keyword:
+                        keyword = keyword.replace(key, libraries_dict[key])
 
+                # Check if the string contains characters and if it already exists(ignore case)
+                if not (keyword.isdigit()):
+                    if any(c.isalpha() for c in keyword) and (keyword not in keywords) and len(keyword) > 1 and (
+                            keyword not in libraries):
+                        if not nlp.vocab[keyword].is_stop:
+                            keywords.append(keyword)
+
+    # Spacy tokens lemmatization
+    # doc_keywords = Doc(nlp.vocab, words=keywords)
+    # keywords= [word.lemma_ for word in doc_keywords]
     return libraries, keywords
 
-if __name__ == "__main__": 
 
-    path=os.getcwd()+'\keras\\tests'
+# Store the values of the libraries, in a dictionary, according to the way that they were imported
+# Returns the libraries full name
+# e.g from keras import backend as k
+#       --> keras.backend is stored and pair value(k,keras.backend) is stored at the dict
+# e.g from keras.models import Model, Sequential
+#       --> keras.models.Model, keras.models.Sequential is stored
+def parse_lines_with_libraries(splitted_line, lib_dict):
+    libraries_full_names = []
+    # Check what's the type of library import
+    if 'as' in splitted_line:
+        if 'from' in splitted_line:
+            library_original_name = splitted_line[splitted_line.index('from') + 1] + '.' + \
+                                    splitted_line[splitted_line.index('import') + 1]
+            lib_dict[splitted_line[splitted_line.index('as') + 1].lower()] = library_original_name
+            libraries_full_names.append(library_original_name)
 
-    #Returns all the paths of the files in a directory
-    #Starts from the files in the directory and then from the directories top bottom 
+        else:
+            library_original_name = splitted_line[splitted_line.index('import') + 1]
+            lib_dict[splitted_line[splitted_line.index('as') + 1].lower()] = library_original_name
+            libraries_full_names.append(library_original_name)
+    elif 'from' in splitted_line:
+        # When import from a library we can import multiple libraries
+        # If multiple libraries were imported, we should store them all
+        libraries_after_import = splitted_line[splitted_line.index('import') + 1:len(splitted_line)]
+        for library in libraries_after_import:
+            library_original_name = splitted_line[splitted_line.index('from') + 1] + '.' + library
+            libraries_full_names.append(library_original_name)
+            lib_dict[library] = library_original_name
+    elif 'import' in splitted_line:
+        library_original_name = splitted_line[splitted_line.index('import') + 1]
+        libraries_full_names.append(library_original_name)
+
+    return libraries_full_names, lib_dict
+
+
+# THIS EXISTS JUST FOR TESTING
+# SHOULD BE DELETED
+if __name__ == "__main__":
+
+    path = os.getcwd() + '\keras\\tests'
+    # Returns all the paths of the files in a directory
+    # Starts from the files in the directory and then from the directories top bottom
     file_paths = []
     for r, d, f in os.walk(path):
         for file in f:
             if '.py' in file:
                 file_paths.append(os.path.join(r, file))
 
-    library,keywords=get_libs_and_keywords(file_paths[7])
+    library, keywords = get_libs_and_keywords(file_paths[8])
 
-    library.sort()
+    # library.sort()
     print(library)
-    #keywords.sort()
+    # keywords.sort()
     print(keywords)
-
-
-    
- 
-
-    
